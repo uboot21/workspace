dfbc0e2903867f7dcc975952dd3c78894e45314b
uboot21 <56710671+uboot21@users.noreply.github.com>
2024-04-12 10:07:51 +0200

    neu

--- 3_Server/Playbooks/ssh_pub.sh
+++ 3_Server/Playbooks/ssh_pub.sh
@@ -8,7 +8,7 @@ sudo chown root:root /root/.ssh/
 sudo chown root:root /root/.ssh/*
 echo "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIC/9+hLVbx6nv+v8nhXYxlRvMrUnywzrfiOxuKMhxeDV ansible.ionos@andrejansen.de"  | sudo tee -a /root/.ssh/authorized_keys >/dev/null
 
-# andre
+# user andre
 mkdir -p /home/andre/.ssh
 sudo touch /home/andre/.ssh/authorized_keys
 sudo chmod 700 /home/andre/.ssh && sudo chmod 600 /home/andre/.ssh/*
